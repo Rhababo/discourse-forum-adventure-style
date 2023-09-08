0811b6873eb6ca4455ac63fdfcbd7bdadd594bba
Rhababo <Wesley.Adams@rhababo.com>
2023-09-08 15:41:10 -0500

    Added outlets for composer and main container

---
+++ javascripts/discourse/connectors/composer-fields/adventure-composer-addon-logic.js
@@ -0,0 +1,15 @@
+import Component from "@glimmer/component";
+import { inject as service } from "@ember/service";
+
+export default class Adventure-Composer-Logic extends Component {
+  @service siteSettings;
+
+  get displayName() {
+    const user = this.args.outletArgs.model.details.created_by;
+    if (this.siteSettings.prioritize_username_in_ux) {
+      return user.username;
+    } else {
+      return user.name;
+    }
+  }
+}
