4ba28d40fc8288618c5b21609b367ef0698c6936
Rhababo <Wesley.Adams@rhababo.com>
2023-09-08 15:43:15 -0500

    removed displayName functions

--- javascripts/discourse/connectors/composer-fields/adventure-composer-addon-logic.js
+++ javascripts/discourse/connectors/composer-fields/adventure-composer-addon-logic.js
@@ -4,12 +4,5 @@ import { inject as service } from "@ember/service";
 export default class Adventure-Composer-Logic extends Component {
   @service siteSettings;
 
-  get displayName() {
-    const user = this.args.outletArgs.model.details.created_by;
-    if (this.siteSettings.prioritize_username_in_ux) {
-      return user.username;
-    } else {
-      return user.name;
-    }
-  }
+  
 }
