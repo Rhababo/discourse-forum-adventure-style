bba38216e1d1ae8062bdceb1a7f7fe08f0b41c9c
Rhababo <Wesley.Adams@rhababo.com>
2023-09-08 15:49:59 -0500

    replaced displayName, updated hbs filetype

--- javascripts/discourse/connectors/above-main-container/adventure-topic-logic.js
+++ javascripts/discourse/connectors/above-main-container/adventure-topic-logic.js
@@ -1,7 +1,14 @@
 import Component from "@glimmer/component";
 import { inject as service } from "@ember/service";
 
-export default class Adventure-Logic extends Component {
+export default class AdventureLogic extends Component {
   @service siteSettings;
-
+  get displayName() {
+    const user = this.args.outletArgs.model.details.created_by;
+    if (this.siteSettings.prioritize_username_in_ux) {
+      return user.username;
+    } else {
+      return user.name;
+    }
+  }
 }
--- javascripts/discourse/connectors/composer-fields/adventure-composer-addon-logic.js
+++ javascripts/discourse/connectors/composer-fields/adventure-composer-addon-logic.js
@@ -1,8 +1,15 @@
 import Component from "@glimmer/component";
 import { inject as service } from "@ember/service";
 
-export default class Adventure-Composer-Logic extends Component {
+export default class AdventureComposerLogic extends Component {
   @service siteSettings;
 
-  
+  get displayName() {
+    const user = this.args.outletArgs.model.details.created_by;
+    if (this.siteSettings.prioritize_username_in_ux) {
+      return user.username;
+    } else {
+      return user.name;
+    }
+  }
 }
